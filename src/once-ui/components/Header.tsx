4cf244fa233aed5b411aaf67955e07d28208c77e
JimmyA32 <jimmyangel12345@gmail.com>
2024-09-12 17:12:24 -0300

    Update Header.tsx

--- src/once-ui/components/Header.tsx
+++ src/once-ui/components/Header.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-export function Header() {
+export default function Header() {
   const NavItem = ({ href, children }: { href: string; children: React.ReactNode }) => (
     <Link 
       href={href} 
